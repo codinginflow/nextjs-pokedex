9b8e43cd9f5e425aec1f72db43962dc82a0a59a2
Coding in Flow <info@codinginflow.com>
2023-04-14 13:12:57 +0200

    End of part 4

---
+++ src/hooks/usePokemon.ts
@@ -0,0 +1,23 @@
+import useSWR from "swr";
+import * as PokemonApi from "@/network/pokemon-api";
+import { AxiosError } from "axios";
+
+export default function usePokemon(name: string) {
+
+    const { data, isLoading } = useSWR(name, async () => {
+        try {
+            return await PokemonApi.getPokemon(name);
+        } catch (error) {
+            if (error instanceof AxiosError && error.response?.status === 404) {
+                return null;
+            } else {
+                throw error;
+            }
+        }
+    });
+
+    return {
+        pokemon: data,
+        pokemonLoading: isLoading,
+    }
+}
--- src/pages/[pokemon].tsx
+++ src/pages/[pokemon].tsx
@@ -1,16 +1,15 @@
 import { useRouter } from "next/router";
-import useSWR from "swr";
-import * as PokemonApi from "@/network/pokemon-api";
 import Head from "next/head";
 import Link from "next/link";
 import { Spinner } from "react-bootstrap";
 import Image from "next/image";
+import usePokemon from "@/hooks/usePokemon";
 
 export default function PokemonDetailsPage() {
     const router = useRouter();
     const pokemonName = router.query.pokemon?.toString() || "";
 
-    const { data: pokemon, isLoading: pokemonLoading } = useSWR(pokemonName, PokemonApi.getPokemon);
+    const { pokemon, pokemonLoading } = usePokemon(pokemonName);
 
     return (
         <>
@@ -21,20 +20,21 @@ export default function PokemonDetailsPage() {
             <div className="d-flex flex-column align-items-center">
                 <p><Link href="/" className="link-light">← PokéDex</Link></p>
                 {pokemonLoading && <Spinner animation="grow" />}
-                {pokemon && 
+                {pokemon === null && <p>Pokemon not found</p>}
+                {pokemon &&
                     <>
-                       <h1 className="text-center text-capitalize">{pokemon.name}</h1> 
-                       <Image
+                        <h1 className="text-center text-capitalize">{pokemon.name}</h1>
+                        <Image
                             src={pokemon.sprites.other["official-artwork"].front_default}
                             alt={"Pokemon: " + pokemon.name}
                             width={400}
                             height={400}
-                       />
-                       <div className="d-inline-block mt-2">
-                        <div><strong>Types:</strong> {pokemon.types.map(type => type.type.name).join(", ")}</div>
-                        <div><strong>Height:</strong> {pokemon.height * 10} cm</div>
-                        <div><strong>Weight:</strong> {pokemon.weight / 10} kg</div>
-                       </div>
+                        />
+                        <div className="d-inline-block mt-2">
+                            <div><strong>Types:</strong> {pokemon.types.map(type => type.type.name).join(", ")}</div>
+                            <div><strong>Height:</strong> {pokemon.height * 10} cm</div>
+                            <div><strong>Weight:</strong> {pokemon.weight / 10} kg</div>
+                        </div>
                     </>
                 }
             </div>
